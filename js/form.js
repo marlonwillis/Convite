c35c00d7bef7238343c58c2b82e3d6cd0f04dded
Marlon Willis <marlon_willis11@yahoo.com.br>
2023-09-29 18:50:45 -0300

    kç

--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('#date-form').mask('00/00/0000');
   $('#Telefone').mask('(00) 00000-0000');
+  $('#Telefonebottom').mask('(00) 00000-0000');
 });
 
 $("input[name='Tipo']").on("click", function(){
