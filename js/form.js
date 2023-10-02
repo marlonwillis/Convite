c582f8376fb16dfc28843529df47b1985f94e087
Marlon Willis <marlon_willis11@yahoo.com.br>
2023-10-02 01:01:01 -0300

    V3

--- js/form.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#date-form').mask('00/00/0000');
-  $('#Telefone').mask('(00) 00000-0000');
-  $('#Telefonebottom').mask('(00) 00000-0000');
-});
-
-$("input[name='Tipo']").on("click", function(){
-    $(".template").toggle(this.value === "Entregador" && this.checked);
-});
